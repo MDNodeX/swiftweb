98b8fefc00301e4c27b2e51131bde20ad6a38b8b
MDNodeX <139620295+MDNodeX@users.noreply.github.com>
2026-05-18 23:57:30 +0600

    Update db.js

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -17,10 +17,11 @@
 import mysql from "mysql2/promise";
 
 export const mySqlDB = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: "mysql-production-d246.up.railway.app",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
-
