feea11dfe4af02602369d290dc30db1998ed8a59
MDNodeX <139620295+MDNodeX@users.noreply.github.com>
2026-05-18 23:34:09 +0600

    Update db.js

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -16,14 +16,12 @@
 
 import mysql from "mysql2/promise";
 
-const mySqlDB = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10,
+export const mySqlDB = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 export { mySqlDB };
