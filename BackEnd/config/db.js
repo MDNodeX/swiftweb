8fe5ca3250178d14da5708829f9130852b1e7a2b
MDNodeX <139620295+MDNodeX@users.noreply.github.com>
2026-05-18 15:13:11 +0600

    Update db.js

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -20,17 +20,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const mySqlDB = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // port: process.env.DB_PORT,
 
-  // host: process.env.MYSQLHOST,
-  // user: process.env.MYSQLUSER,
-  // password: process.env.MYSQLPASSWORD,
-  // database: process.env.MYSQLDATABASE,
-  // port: process.env.MYSQLPORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 
   waitForConnections: true,
   connectionLimit: 10,
